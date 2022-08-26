ad4095d946f5eacc4bd25d1b5f43bd3a7980c533
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-08-26 19:00:45 +0900

    ✨ 簡易的なAPIを作成

--- app/main.py
+++ app/main.py
@@ -1,13 +1,47 @@
-from typing import Union
+import datetime
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+class Plan(BaseModel):
+    plan_id: str
+    plan_name: str = Field(max_length=40)
+    start_date: datetime.date
+    end_date: datetime.date
+    verify_key: str
+    email: str
+    timestamp: datetime.date
+
+class Spot(BaseModel):
+    spot_id: int
+    plan_id: str
+    spot_name: str = Field(max_length=40)
+    image: str
+    url: str
+    priority: bool
+    visited: bool
+    icon: str
+
+class Memo(BaseModel):
+    memo_id: int
+    spot_id: int
+    text: str = Field(max_length=150)
+    marked: str
+
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "HelloWorld"}
 
+@app.post("/plans")
+async def plans(plans: Plan):
+    return {"plans": plans}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/spots")
+async def spots(spots: Spot):
+    return {"spots": spots}
+
+@app.post("/memos")
+async def memos(memos: Memo):
+    return {"memos": memos}
