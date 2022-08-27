ab38390901afb56cbd7f9ab57f99c065e380d572
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-08-27 13:57:43 +0900

    🚑  SQLAlchemyからHeroku Postgresへアクセスするための設定を追記

--- app/database.py
+++ app/database.py
@@ -16,11 +16,17 @@ DB_NAME = os.environ['DB_NAME']
 
 DATABASE_URL = '{}://{}:{}@{}:{}/{}'.format(DATABASE, USER, PASSWORD, HOST, PORT, DB_NAME)
 
+# SQLAlchemyからHeroku Postgresへアクセスするための処理
+# https://help.heroku.com/ZKNTJQSK/why-is-sqlalchemy-1-4-x-not-connecting-to-heroku-postgres
+uri = os.getenv(DATABASE_URL)
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 # charsetの設定など
-engine = create_engine(DATABASE_URL)
+engine = create_engine(uri)
 
 # autocommit, autoflushに何を設定すべきか
-session = sessionmaker(bind=engine)
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # 継承してModelを作成するためのインスタンス
 Base = declarative_base()
