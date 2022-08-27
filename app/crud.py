7a7f30c6c8362df3c6ea385c443de49a89fd9a02
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-08-27 13:22:37 +0900

    ✨ データベース操作の簡易的な関数を作成

---
+++ app/crud.py
@@ -0,0 +1,55 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+# プラン一覧取得
+def get_plans(db: Session):
+    return db.query(models.Plan).all()
+
+# スポット一覧取得
+def get_spots(db: Session):
+    return db.query(models.Spot).all()
+
+# メモ一覧取得
+def get_memos(db: Session):
+    return db.query(models.Memo).all()
+
+# プラン登録
+def create_plan(db: Session, plan: schemas.Plan):
+    db_plan = models.Plan(
+        plan_id = plan.plan_id,
+        plan_name = plan.plan_name,
+        start_date = plan.start_date,
+        end_date = plan.end_date,
+        verify_key = plan.verify_key,
+        email = plan.email,
+        timestamp = plan.timestamp
+    )
+    db.add(db_plan)
+    db.commit()
+    db.refresh(db_plan)
+
+# スポット登録
+def create_spot(db: Session, spot: schemas.Spot):
+    db_spot = models.Spot(
+        plan_id = spot.plan_id,
+        spot_name = spot.spot_name,
+        image = spot.image,
+        url =  spot.url,
+        priority = spot.priority,
+        visited = spot.visited,
+        icon = spot.icon
+    )
+    db.add(db_spot)
+    db.commit()
+    db.refresh(db_spot)
+
+# メモ登録
+def create_memo(db: Session, memo: schemas.Memo):
+    db_memo = models.Memo(
+        spot_id = memo.spot_id,
+        text = memo.text,
+        marked = memo.marked,
+    )
+    db.add(db_memo)
+    db.commit()
+    db.refresh(db_memo)
